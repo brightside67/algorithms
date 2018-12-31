15969290af7e3a9e075e2daeb4f1434bdc706ced
brightside67 <mobrika@list.ru>
2019-01-01 02:07:45 +0300

    prehook works

--- add.ts
+++ add.ts
@@ -1,10 +1,10 @@
 function add2(...param1: number[]): number {
 	let total = 0;
 	param1.forEach(num => {
-		total += bawtfm2233;
+		total += num;
 	});
-	console.table(1122);
-	return aga;
+
+	return total;
 }
-console.log(add2(1, 2, 3, 4, 5));
-//				console.log(add(3, 2))
+add2(1, 2, 3, 4, 5);
+// console.log(add(3, 2))
