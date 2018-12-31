713724ebfeb460691cc479273fdea533b52cd53b
brightside67 <mobrika@list.ru>
2018-12-31 23:52:35 +0300

    add prettier config

--- absoluteMin.ts
+++ absoluteMin.ts
@@ -1,7 +1,9 @@
 function add(a: number[]): number {
-  const isEven = a.length % 2 === 0;
+	const isEven = a.length % 2 === 0;
 
-  return isEven ? a[a.length / 2 - 1] : a[Math.floor(a.length / 2)];
+	return isEven
+		? a[a.length / 2 - 1]
+		: a[Math.floor(a.length / 2)];
 }
 
 console.log(add([2, 4, 7]));
