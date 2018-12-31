57c54c049404526e66838dcba0c74a059d5d8a82
brightside67 <mobrika@list.ru>
2019-01-01 01:21:57 +0300

    dad

--- add.ts
+++ add.ts
@@ -3,7 +3,7 @@ function add2(...param1: number[]): number {
 	param1.forEach(num => {
 		total += bawtfm;
 	});
-	console.table(dada2);
+	console.table(dada2333);
 	return tt;
 }
 console.log(add2(1, 2, 3, 4, 5));
