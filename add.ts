9534f0b3052636339a4d21db08f03056145a91ad
brightside67 <mobrika@list.ru>
2019-01-01 01:41:52 +0300

    dada

--- add.ts
+++ add.ts
@@ -6,5 +6,5 @@ function add2(...param1: number[]): number {
 				console.table(1122);
 return aga;
 }
-console.log(add2(1, 2, 3, 4, 5));
+		console.log(add2(1, 2, 3, 4, 5));
 //console.log(add(3, 2))
