bbe2a74581135854fe48ba33086b9af1665ab8c3
brightside67 <mobrika@list.ru>
2019-01-01 01:37:05 +0300

    dadada

--- add.ts
+++ add.ts
@@ -1,10 +1,10 @@
 function add2(...param1: number[]): number {
 	let total = 0;
-	param1.forEach(nu2m => {
-		total += bawtfm;
+	param1.forEach(num => {
+		total += bawtfm22;
 	});
-	console.table(dada2332223);
-	return tt;
+	console.table(1122);
+	return aga;
 }
 console.log(add2(1, 2, 3, 4, 5));
 //console.log(add(3, 2))
