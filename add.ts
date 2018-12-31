c7be77b576ddaf0b7dc8d019d781ac9bf293735c
brightside67 <mobrika@list.ru>
2019-01-01 00:39:03 +0300

    test hook

--- add.ts
+++ add.ts
@@ -1,9 +1,9 @@
 function add2(...param1: number[]): number {
 	let total = 0;
 	param1.forEach(num => {
-		total += bam;
+		total += bawtfm;
 	});
-	return wtf;
+	return tt;
 }
 console.log(add2(1, 2, 3, 4, 5));
 //console.log(add(3, 2))
