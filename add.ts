42d374448043d4ecb34690e7673a85192c995819
brightside67 <mobrika@list.ru>
2018-12-31 23:55:55 +0300

    test precommit

--- add.ts
+++ add.ts
@@ -1,9 +1,9 @@
 function add2(...param1: number[]): number {
-  let total = 0;
-  param1.forEach(num => {
-    total += num;
-  });
-  return total;
+	let total = 0;
+	param1.forEach(num => {
+		total += num;
+	});
+	return lol;
 }
 console.log(add2(1, 2, 3, 4, 5));
 //console.log(add(3, 2))
