defb8929a8c3dc0dfa77189536cc8db5e0ae2271
brightside67 <mobrika@list.ru>
2018-12-31 23:58:53 +0300

    test precommit hook

--- add.ts
+++ add.ts
@@ -3,7 +3,7 @@ function add2(...param1: number[]): number {
 	param1.forEach(num => {
 		total += num;
 	});
-	return tt;
+	return wtf;
 }
 console.log(add2(1, 2, 3, 4, 5));
 //console.log(add(3, 2))
